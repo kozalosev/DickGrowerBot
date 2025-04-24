db2e79ba9abb5aa6302f370b9b5205e0a4795825
tdc <tdc@sudo.host>
2025-04-24 18:18:44 +0800

    remove web page preview

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -17,6 +17,7 @@ use rust_i18n::t;
 use teloxide::Bot;
 use teloxide::payloads::{AnswerCallbackQuerySetters, SendMessage};
 use teloxide::requests::{JsonRequest, Requester};
+use teloxide::sugar::request::RequestLinkPreviewExt;
 use teloxide::types::{CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Message, ReplyParameters};
 use teloxide::types::ParseMode::Html;
 
@@ -115,7 +116,7 @@ impl <D: CallbackDataWithPrefix> HandlerImplResult<D> {
 
 pub fn reply_html<T: Into<String>>(bot: Bot, msg: &Message, answer: T) -> JsonRequest<SendMessage> {
     // TODO: split to several messages if the answer is too long
-    let mut answer = bot.send_message(msg.chat.id, answer);
+    let mut answer = bot.send_message(msg.chat.id, answer).disable_link_preview(true);
     answer.parse_mode = Some(Html);
     if msg.chat.is_group() || msg.chat.is_supergroup() {
         answer.reply_parameters.replace(ReplyParameters::new(msg.id));
